d731ecba6bbf0a8022097e0946540e77da2f21db
decca1011 <deepakpatil101197@gmail.com>
2023-11-19 16:36:04 +0530

    Build the frontend of the chat Application

---
+++ Public/Profile/Interface.js
@@ -0,0 +1,41 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const usersContainer = document.getElementById("users");
+    const messagesContainer = document.getElementById("messages");
+    const messageInput = document.getElementById("message-input");
+    const sendButton = document.getElementById("send-button");
+
+    // Mock data, replace with your actual user data from the backend
+    const currentUser = { username: "User1" };
+    const onlineUsers = [{ username: "User2" }, { username: "User3" }];
+
+    // Function to display a new message in the chat
+    function displayMessage(message) {
+        const li = document.createElement("li");
+        li.textContent = message;
+        messagesContainer.appendChild(li);
+    }
+
+    // Function to display online users
+    function displayOnlineUsers() {
+        usersContainer.innerHTML = "";
+        onlineUsers.forEach(user => {
+            const li = document.createElement("li");
+            li.textContent = user.username;
+            usersContainer.appendChild(li);
+        });
+    }
+
+    // Display online users when the page loads
+    displayOnlineUsers();
+
+    // Event listener for the send button
+    sendButton.addEventListener("click", function () {
+        const message = messageInput.value;
+        if (message.trim() !== "") {
+            // For simplicity, just display the message locally
+            displayMessage(`${currentUser.username}: ${message}`);
+            messageInput.value = "";
+        }
+    });
+});
